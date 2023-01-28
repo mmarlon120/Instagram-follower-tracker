c9363bc9de0d755ed8982bdf16f727e0e82c419f
Marlon Martinez <kesky120@gmail.com>
2023-01-28 16:26:29 -0500

    Adds the list_organizor function
    The point of this function is to show 2 lists visually
    The first list shows the specific new followers you gained since
    the last iteration
    The second list shows the followers that were lost since the last
    iteration

--- Followertracker.py
+++ Followertracker.py
@@ -28,7 +28,7 @@ def main():
 # If you're the owner of test1 or test2 then you're already here :)
     if username == "test1":
         old_list = ["Elliot","Josh","Nick"]
-        new_list = ["Dylan","King Touchy","Sage", "Katana"]
+        new_list = ["Dylan","God King Touchy","Sage", "Katana"]
         print(comparitor(old_list, new_list))
         exit(0)
     elif username == "test2":
@@ -56,7 +56,29 @@ def main():
             new_data = f.read()
             new_list = new_data.split("\n")
         print(comparitor(old_list, new_list))
+        exit(0)
 
+#List Organizer is to format the information given to comparitor
+#The Format should come out as "Old List | New List"
+def list_organizer(old, new, difference):
+    string = ""
+    if difference > 0:
+        for follower in range(difference):
+            old.append(" ")
+    else:
+        difference = difference * -1
+        for follower in range(difference):
+            new.append(" ")
+    new.sort(key = len, reverse = True)
+    lengthkey = len(new[0])
+    for follower in new:
+        if len(follower) < lengthkey:
+            newfollower = follower.rjust(lengthkey, " ")
+            print(f"{newfollower}|{old[new.index(follower)]}")
+        else:
+            print(f"{follower}|{old[new.index(follower)]}")
+#Comparator takes the raw following information taken from 
+#the old and new followers.txt and extracts useful information
 def comparitor(old, new):
     if old == new:
         return "There has been no change to your follower count"
@@ -65,6 +87,7 @@ def comparitor(old, new):
             new.remove(follower)
             old.remove(follower)
     net = len(new) - len(old)
-    return f"Followers gained/lost: {net}"
+    print(f"Followers Gained|Lost: {net}")
+    list_organizer(old, new, net)
 
 main()
